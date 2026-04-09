f1c2e2ab9658fc0f0494045be567912a1f449ca2
Mateus Weiss Medeiros <mateuswmedeiros@gmail.com>
2026-04-09 10:37:47 -0300

    Código que para caso primeira letra da palavra inserida começar com vogal

--- Estruturas de Repetição/For.py
+++ Estruturas de Repetição/For.py
@@ -1,9 +1,17 @@
-import time
+# import time
 
-for i in range(10, 0, -1):
-    print(i)
-    time.sleep(1)
+# for i in range(10, 0, -1):
+#     print(i)
+#     time.sleep(1)
 
-for i in range (0, 11):
-    print (i)
-    time.sleep(1)
+# for i in range (0, 11):
+#     print (i)
+#     time.sleep(1)
+
+vogais = ['a','e','i','o','u']
+palavra = input("Digite uma palavra que começe com consoante: ")
+for letra in palavra:
+    primeira_letra = palavra[0].lower()
+    if primeira_letra in vogais:
+        break
+    print (letra)
